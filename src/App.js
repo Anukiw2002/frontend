be5ca1eb2db73acc4a16a368c0cecc0ac5118dc3
man25-dotcom <Maahelaw@gmail.com>
2024-08-05 15:10:19 +0530

    Random

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddInventory from "./components/AddInventory";
 import AddProduct from "./components/AddProducts";
+import AddCustomer from  "./components/AddCustomers";
 import NavBar from "./components/NavBar";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="add-products" element={<AddProduct />} />
         <Route path="add-inventory" element={<AddInventory />} />
+        <Route path="customers" element={<AddCustomer />} />
       </Routes>
     </Router>
   );
